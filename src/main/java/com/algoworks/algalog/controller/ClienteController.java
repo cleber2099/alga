0354518381ea1eac4951afd28c7b7c3fecb260e2
cleber2099 <cleber.santos.2099@gmail.com>
2023-03-20 11:42:54 -0300

    implementado metodo de leitura  do banco do dados mas sem sucesso

--- src/main/java/com/algoworks/algalog/controller/ClienteController.java
+++ src/main/java/com/algoworks/algalog/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package com.algoworks.algalog.controller;
 
 import domain.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,20 +11,13 @@ import java.util.List;
 
 @RestController
 public class ClienteController {
+    @PersistenceContext
+    private EntityManager manager;
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("joao");
-        cliente1.setTelefone("81 99999 9999");
-        cliente1.setEmail("joaodascouves@.gmail");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("maria");
-        cliente2.setTelefone("81 88888 9999");
-        cliente2.setEmail("maria@.gmail");
-        return Arrays.asList(cliente1,cliente2);
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
+
 }
 
--- src/main/java/domain/model/Cliente.java
+++ src/main/java/domain/model/Cliente.java
@@ -1,6 +1,21 @@
 package domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter
+@Getter
+@Entity
 public class Cliente {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
@@ -8,35 +23,5 @@ public class Cliente {
 
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
